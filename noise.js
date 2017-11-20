86641fe1e045f605afd04877551b2510bec78125
Canaan West <canaan.west@gmail.com>
2017-11-20 14:26:52 -0800

    basic functionality

--- noise.js
+++ noise.js
@@ -1,3 +1,31 @@
+/*eslint-disable*/
+
+// function setupHelp(class){
+//   helpText = [
+//   {"note a": "aAudio"},
+//   {"note b": "bAudio"},
+//   {"note c": "cAudio"},
+//   {"note d": "dAudio"},
+//   {"note e": "eAudio"},
+//   {"note f": "fAudio"},
+//   {"note g": "gAudio"}
+//   ]
+//   return helpText[class]
+// }
+
 $(document).ready( function() {
-  // your code here
+  getID =
+  {"note a": "#aAudio",
+  "note b": "#bAudio",
+  "note c": "#cAudio",
+  "note d": "#dAudio",
+  "note e": "#eAudio",
+  "note f": "#fAudio",
+  "note g": "#gAudio"}
+
+  $('.instrument').on('click', '.note', function(){
+    let audio = $(getID[$(this).attr('class')])[0]
+    audio.load()
+    audio.play()
+  })
 });
