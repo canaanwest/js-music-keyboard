03a8ce50b8d6cc3dabfe237072c54db88c75b117
Canaan West <canaan.west@gmail.com>
2017-11-20 14:28:05 -0800

    adding pause/reload functionality for keyup

--- noise.js
+++ noise.js
@@ -1,18 +1,5 @@
 /*eslint-disable*/
 
-// function setupHelp(class){
-//   helpText = [
-//   {"note a": "aAudio"},
-//   {"note b": "bAudio"},
-//   {"note c": "cAudio"},
-//   {"note d": "dAudio"},
-//   {"note e": "eAudio"},
-//   {"note f": "fAudio"},
-//   {"note g": "gAudio"}
-//   ]
-//   return helpText[class]
-// }
-
 $(document).ready( function() {
   getID =
   {"note a": "#aAudio",
@@ -27,5 +14,21 @@ $(document).ready( function() {
     let audio = $(getID[$(this).attr('class')])[0]
     audio.load()
     audio.play()
+  })
+
+  $('body').keydown(function(key) {
+    let press = `note ${key.key}`
+
+    let audio = $(getID[press])[0]
+    // audio.load()
+    audio.play()
+  })
+
+  $('body').keyup(function(key){
+      let press = `note ${key.key}`
+
+    let audio = $(getID[press])[0]
+    audio.pause()
+    audio.load()
   })
 });
