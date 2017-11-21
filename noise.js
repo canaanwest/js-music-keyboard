c93b3a185d12b8a495268396d3fd22899ce384e3
Canaan West <canaan.west@gmail.com>
2017-11-20 16:35:37 -0800

    adding changing background colors

--- noise.js
+++ noise.js
@@ -1,34 +1,45 @@
 /*eslint-disable*/
 
 $(document).ready( function() {
-  getID =
-  {"note a": "#aAudio",
+  const getID = {
+  "note a": "#aAudio",
   "note b": "#bAudio",
   "note c": "#cAudio",
   "note d": "#dAudio",
   "note e": "#eAudio",
   "note f": "#fAudio",
-  "note g": "#gAudio"}
+  "note g": "#gAudio"
+};
 
-  $('.instrument').on('click', '.note', function(){
+  getColor = {
+    "#aAudio": "#a13fad",
+    "#bAudio": "#f26fd4",
+    "#cAudio": "#3e181b",
+    "#dAudio": "#f0421c",
+    "#eAudio": "#fec02d",
+    "#fAudio": "#20d071",
+    "#gAudio": "#1a9ddb",
+  };
+
+  $('.note').on('click', function(){
     let audio = $(getID[$(this).attr('class')])[0]
     audio.load()
     audio.play()
+    $('body').css('background-color', `${getColor[getID[$(this).attr('class')]]}`)
   })
 
   $('body').keydown(function(key) {
     let press = `note ${key.key}`
-
     let audio = $(getID[press])[0]
-    // audio.load()
     audio.play()
   })
 
   $('body').keyup(function(key){
-      let press = `note ${key.key}`
+    let press = `note ${key.key}`;
 
-    let audio = $(getID[press])[0]
-    audio.pause()
-    audio.load()
+
+    let audio = $(getID[press])[0];
+    audio.pause();
+    audio.load();
   })
 });
